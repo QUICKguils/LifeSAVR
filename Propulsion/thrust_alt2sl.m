2de6493658540738cbb40eff2250cf592abe3087
QUICKguils <guilainsjc@gmail.com>
2023-03-30 23:28:39 +0200

    Make thrust_alt2sl independent of MAT files.

--- Propulsion/thrust_alt2sl.m
+++ Propulsion/thrust_alt2sl.m
@@ -1,24 +1,21 @@
-function [alt_over_sl] = thrust_alt2sl(M, p)
+function [alt_over_sl] = thrust_alt2sl(TAS, h, BPR, G)
 % thrust_sl2alt  Thrust conversion from sea level to h.
 
-%% Imports
+% Make Utils/ISA visible.
+addpath(genpath(fullfile(fileparts(mfilename("fullpath")), "../Utils")));
 
-% Directory where the present file lies.
-file_dir = fileparts(mfilename("fullpath"));
+% Compute air properties at altitude h and at sea level.
+% NOTE: we don't load constants.mat in this function, for speed.
+[~, p,    ~, a] = ISA(h);
+[~, p_sl, ~, ~] = ISA(0);
 
-% Constants and aircraft data.
-C = load(fullfile(file_dir, "../constants.mat"));
-load(fullfile(file_dir, "../data.mat"), "Propu");
-BPR = Propu.engine.BPR;
-G = Propu.engine.G;
-
-% Utilities.
-addpath(genpath(fullfile(file_dir, "../Utils")));
+% Retrieve Mach number, for the given flight condition (TAS, h).
+M = TAS / a;
 
 % Preterms computation.
-A = -0.4327 * (C.p_cr/C.p_sl)^2 + 1.3855 * C.p_cr/C.p_sl + 0.0472;
-X =  0.1377 * (C.p_cr/C.p_sl)^2 - 0.4374 * C.p_cr/C.p_sl + 1.3003;
-Z =  0.9106 * (C.p_cr/C.p_sl)^2 - 1.7736 * C.p_cr/C.p_sl + 1.8697;
+A = -0.4327 * (p/C.p_sl)^2 + 1.3855 * p/p_sl + 0.0472;
+X =  0.1377 * (p/C.p_sl)^2 - 0.4374 * p/p_sl + 1.3003;
+Z =  0.9106 * (p/C.p_sl)^2 - 1.7736 * p/p_sl + 1.8697;
 
 alt_over_sl = A ...
 	- Z * p/C.p_sl * (0.377*(1+BPR)*M) / sqrt((1+0.82*BPR)*G) ...
